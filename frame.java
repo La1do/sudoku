c57e592a131a67d4e1bf364bbf6a426f6c2fe10a
Truong <baotruong11298@gmail.com>
2024-09-19 21:18:56 +0700

    add icon in frame.java file

--- frame.java
+++ frame.java
@@ -3,13 +3,17 @@ import java.awt.*;
 
 public class frame {
     public static void main(String[] args) {
+
+
+        ImageIcon icon = new ImageIcon("icon.jpg");
         board board = new board();
         board.setBackground(Color.black);
         JFrame frame = new JFrame();
+        frame.setIconImage(icon.getImage());
         frame.setSize(1024, 720);
-        frame.setTitle("sudoku test");
+        frame.setTitle("Sudoku");
         frame.getContentPane().setBackground(Color.black);
-
+        frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.setLayout(null);
