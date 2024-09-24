ac93f799f7b262eb7f4dbdfccfa3eb217ffdc8db
La1do <111567514+La1do@users.noreply.github.com>
2024-09-24 22:14:34 +0700

    Update frame.java

--- frame.java
+++ frame.java
@@ -1,31 +1,52 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class frame {
+public class frame extends JFrame implements ActionListener {
+
     public static void main(String[] args) {
+       new frame();
+    }
+    board board = new board();
 
-
+    frame(){
         ImageIcon icon = new ImageIcon("icon.jpg");
         // panels
         //board
-        board board = new board();
+
         board.setBackground(Color.black);
         // button
-        Button_Right_Side Right_Side = new Button_Right_Side();
+        Button_Right_Side Right_Side = new Button_Right_Side(this);
         Right_Side.setBackground(Color.black);
-        JFrame frame = new JFrame();
-        frame.setIconImage(icon.getImage());
-        frame.setSize(1024, 720);
-        frame.setTitle("Sudoku");
-        frame.getContentPane().setBackground(Color.black);
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame config
+        this.setIconImage(icon.getImage());
+        this.setSize(1024, 720);
+        this.setTitle("Sudoku");
+        this.getContentPane().setBackground(Color.black);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        frame.setLayout(null);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-        frame.add(board);
-        frame.add(Right_Side);
+        this.setLayout(null);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        this.add(board);
+        this.add(Right_Side);
 
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton source = (JButton) e.getSource();
+        if (source.getText().equals("Validate")) {
+            System.out.println("Validate button clicked");
+        } else if (source.getText().equals("Hint")) {
+            System.out.println("Hint button clicked");
+        } else if (source.getText().equals("Number-First Input")) {
+            System.out.println("Number-First Input button clicked");
+        } else if (source.getText().equals("Notes")) {
+            System.out.println("Notes button clicked");
+        }
+    }
+
 }
